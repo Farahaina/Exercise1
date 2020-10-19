ae8c223638fb691fcaeabd3d550fbb95d28f74b2
Nur Farahaina <farahaina444@gmail.com>
2020-10-20 01:51:11 +0800

    first commit for exercise1

---
+++ Driver.java
@@ -0,0 +1,21 @@
+//Author : Nur Farahaina
+public class Driver{
+
+	public static void main (String [] arg) {
+		//Fish actually is a data type
+		//Fish nemo; == int x;
+		Fish nemo = new Fish(); //nemo is object, Fish is class
+		nemo.Swim();
+		nemo.color = "red"; //tukar value
+		nemo.Swim();
+
+		Fish dory = new Fish(); //another object
+		dory.Swim();
+		dory.color = "yellow";
+		dory.Swim();
+
+		
+	}
+
+
+}
