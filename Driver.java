3f2ee9b06b3b095839e1608d304f9dec8d4d0189
Farahaina <73125774+Farahaina@users.noreply.github.com>
2020-10-20 10:13:43 +0800

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -6,13 +6,28 @@ public class Driver{
 		//Fish nemo; == int x;
 		Fish nemo = new Fish(); //nemo is object, Fish is class
 		nemo.Swim();
-		nemo.color = "red"; //tukar value
+		//nemo.color = "red"; //tukar value
+		nemo.SetColor("red");
 		nemo.Swim();
 
-		Fish dory = new Fish(); //another object
-		dory.Swim();
-		dory.color = "yellow";
-		dory.Swim();
+
+		//nemo.Eat();  //superclass call subclass method
+
+
+		Shark fiery = new Shark();
+		fiery.Eat();
+		fiery.Swim();
+		fiery.SetSharpTeeth(130);
+		fiery.Eat();
+
+		//Fish dory = new Fish(); //another object
+		//dory.Swim();
+	    //dory.color = "yellow";
+		//dory.SetColor("yellow");
+		//dory.Swim();
+
+		//Aquarium fancyAquarium = new Aquarium();
+		//fancyAquarium.fillFish();
 
 		
 	}
